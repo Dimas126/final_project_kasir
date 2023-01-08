15f2a4b6514c5492c29586546d7f71af74349759
Dimas Aandalan 126 <122203586+Dimas126@users.noreply.github.com>
2023-01-08 22:40:57 +0700

    Create kelompok1cust.java
    KELOMPOK 1 FRIENDLY
    : )

---
+++ kelompok1cust.java
@@ -0,0 +1,19 @@
+public class cust {
+    protected String nama;
+    protected String telp;
+
+    public String getNama(){
+        return this.nama;
+    }
+    public void setNama(String a){
+        this.nama = a;
+    }
+
+    public String getTelp(){
+        return this.telp;
+    }
+   
+    public void setTelp(String b){
+        this.telp = b;
+    }
+}
