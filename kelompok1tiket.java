35c2630db95b995ff60a8992a3ca09597614a494
Dimas Aandalan 126 <122203586+Dimas126@users.noreply.github.com>
2023-01-08 22:49:24 +0700

    Create kelompok1tiket.java
    nah pada program ketiga saya memakai private yang gunanya sendiri untuk pengkhususan pengaksesan yang hanya bisa  digunakan oleh class itu sendiri.

---
+++ kelompok1tiket.java
@@ -0,0 +1,25 @@
+public class tiket extends cust {
+
+    private String kode;
+    private int harga;
+    private String jam;
+
+    public String getKode(){
+        return this.kode;
+    }
+    public void setKode(String kd){
+        this.kode = kd;
+    }
+    public int getHarga(){
+        return this.harga;
+    }
+    public void setHarga(int hg){
+        this.harga = hg;
+    }
+    public String getJam(){
+        return this.jam;
+    }
+    public void setJam(String waktu){
+        this.jam = waktu;
+    }
+}
